bc7bb71a277a2827debc301cd1629eccebed91ba
Bahermohamd <105526548+Bahermohamd@users.noreply.github.com>
2024-06-24 21:26:58 +0300

    Update init-packages.el

--- init-packages.el
+++ init-packages.el
@@ -9,7 +9,7 @@
 
 ; list the packages you want
 (setq package-list
-      '( yasnippet windresize auto-complete auto-complete-c-headers ac-c-headers smex color-theme-sanityinc-solarized cyberpunk-theme))
+      '( yasnippet catppuccin-theme auto-complete auto-complete-c-headers ac-c-headers smex color-theme-sanityinc-solarized cyberpunk-theme))
 
 
 ; activate all the packages
